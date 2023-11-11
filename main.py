87b1aa68bbe5529c60caa6fc460823bc3ca99a88
LeonoffNSU <k.leonov@g.nsu.ru>
2023-11-11 17:29:19 +0700

    begin

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 with open('Generation-П.txt', encoding='utf-8') as text_lit:
     text_str = text_lit.read()
-    text_str = text_str.replace('', '')
-    print(text_str.find(''))
-    print(text_str)
+
+text_str = text_str.replace('', '')
+list_of_words = list(set(text_str.split()))
+list_of_words.remove('–')
+print(list_of_words)
