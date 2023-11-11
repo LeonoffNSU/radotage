2caf913cc7975b3609c455aef29ffa87435bc523
LeonoffNSU <k.leonov@g.nsu.ru>
2023-11-11 15:45:15 +0700

    begin

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
-print(5)
+with open('Generation-П.txt', encoding='utf-8') as text_lit:
+    text_str = text_lit.read()
+    text_str = text_str.replace('', '')
+    print(text_str.find(''))
+    print(text_str)
